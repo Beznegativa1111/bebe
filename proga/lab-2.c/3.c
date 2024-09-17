c346660c2aaed2f6b3800976211d4917555093f7
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-17 19:37:40 +0700

    Update 3.c

--- proga/lab-2.c/3.c
+++ proga/lab-2.c/3.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+  int a,b,c,d = 0;
+
+  printf("Please Enter the 4 Numbers\n");
+    printf("Number");
+    scanf("%d", &a);
+    if (a < 0) {
+      a = 0;
+    }
+    printf("Number");
+    scanf("%d", &b);
+        if (b < 0) {
+      b = 0;
+    }
+    printf("Number");
+    scanf("%d", &c);
+        if (c < 0) {
+      c = 0;
+    }
+    printf("Number");
+    scanf("%d", &d);
+        if (d < 0) {
+      d = 0;
+    }
+    printf("\n After replace = %d", a);
+    printf("\n After replace = %d", b);
+    printf("\n After replace = %d", c);
+    printf("\n After replace = %d", d);
