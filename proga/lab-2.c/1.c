bd39c7d7b9d3f6656d87806d91b1f2b536a04c0d
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-17 16:37:18 +0700

    Update 1.c

--- proga/lab-2.c/1.c
+++ proga/lab-2.c/1.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    float a,b,c,d,e,f,res1,res2,res3;
+    printf("Input");
+    scanf("%f %f %f %f %f %f",&a, &b, &c,&d,&e,&f);
+    res1 = a + b + c + d +e +f;
+    res2 = a * b * c * d *e *f;
+    if(res1 > res2){
+        res3 = res1 - res2;
+        printf("сумма больше на %f",res3);
+    }
+    else{
+        res3 = res2 - res1;
+        printf("произведение больше на %f",res3);
+    }
+    return 0;
+    //2
+    
+}
