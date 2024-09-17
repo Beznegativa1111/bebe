ba38adf335b58bba1f9991dbd2331f3e389649ab
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-17 20:09:52 +0700

    Update 5.c

--- proga/lab-2.c/5.c
+++ proga/lab-2.c/5.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+int main(){
+    float num;
+    float min_num = 100000;
+    for(int i = 0;i <= 3;i++){
+        printf("введите"); scanf("%f",&num);
+        if(num < min_num){
+            min_num = num;
+        }
+    }
+    printf("%f",min_num);
+}
