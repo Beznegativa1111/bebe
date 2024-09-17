de696a771bd0b42af806bb25fd6d17dd23b5fcd4
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-17 19:20:12 +0700

    Update 2.c

--- proga/lab-2.c/2.c
+++ proga/lab-2.c/2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+   float num,sum = 0;
+   for(int i = 0; i <= 4; i++){
+       printf("input"); scanf("%f",&num);
+       if(num > 0){
+           sum += num;
+       }
+    }
+    printf("%f",sum);
+}
