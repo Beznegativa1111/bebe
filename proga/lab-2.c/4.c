23078dc91936006a2837b48a510613b74fe2b65d
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-17 19:49:00 +0700

    Update 4.c

--- proga/lab-2.c/4.c
+++ proga/lab-2.c/4.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    float num,pl,mn = 0.0;
+    for(int i = 0; i <= 7; i++){
+        printf("введите числа"); scanf("%f",&num);
+        if(num > 0){
+            pl += 1.0;
+        }else{
+            mn += 1.0;
+        }
+    }
+    printf("%f\n",pl);
+    printf("%f",mn);
+}
