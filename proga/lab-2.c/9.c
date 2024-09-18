3afcb69d4a2083a033256c03c7f53f9fbbe3f2bf
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-18 14:42:33 +0700

    Update 9.c

--- proga/lab-2.c/9.c
+++ proga/lab-2.c/9.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    int max = 0;
+    int max_2;
+    int max_m;
+    for(int i = 0; i <= 3; i++){
+         scanf("%d",&num);
+            if(num > max){
+                max = num;
+            }
+    }printf("%d",max);
+    for(int j = 0; j <= 3; j ++){
+            scanf("%d",&num);
+            if(num > max_2 && num != max){
+                max_2 = num;
+            }
+          
+        }printf(" %d",max_2); 
+        max_m = max;
+        max = max_2;
+        max_2 = max_m;
+        
+    printf("%d %d",max,max_m);
+}
 
