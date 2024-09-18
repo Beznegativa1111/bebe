2bcc250501a148eedfc184b095e01b9db6d93dba
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-18 13:44:47 +0700

    Update 6.c

--- proga/lab-2.c/6.c
+++ proga/lab-2.c/6.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    int num;
+    int min = 10000;
+    int max = 0;
+    for(int i = 0;i <= 3;i++){
+        printf("Введите числа"); scanf("%d",&num);
+        if(num < min ){
+            min = num;
+            max = num;
+        }
+        if(num > max){
+            max = num;
+        }
+    }
+    printf("%d %d",min, max);
+
+}
