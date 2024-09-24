810d8c2fba7de9d5b8226a0d3ff71e4e8f47ca17
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-09-24 16:30:39 +0700

    Update 1.c

--- proga/lab-3.c/1.c
+++ proga/lab-3.c/1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(){
+    int a;
+    printf("введите номер месяца:"); scanf("%d", &a);
+    switch(a){
+        case(1):case(2):printf("первый квартал, зима");break;
+        case(3):printf("первый квартал, весна");break;
+        case(4):case(5):printf("2 квартал, весна");break;
+        case(6):printf("2 квартал,лето");break;
+        case(7):case(8):printf("3 квартал, лето");break;
+        case(9):printf("3 квартал, осень");break;
+        case(10):case(11):printf("4 квартал,осень");break;
+        case(12):printf("4 квартал, зима");break;
+        default:printf("нету такого месяца");
+    }
+
+    return 0;
+
+}
