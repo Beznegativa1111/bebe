a2f073f4ed4a16a4cad97d95c0fcc25db39b80a2
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-10-02 13:24:08 +0700

    Update 1-2-3.c

--- proga/lab-4/1-2-3.c
+++ proga/lab-4/1-2-3.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{   //1
+    int s = 0;
+    for(int i = 0; i <= 117; i++){
+        s += i;
+    }
+    printf("%d\n",s);
+    
+    //2
+    double S = 0.0;
+    for(int i = 1; i <= 25;i++){
+        S += sin(i);
+    }
+    printf("%f\n",S);
+    
+    //3
+    double x = 0.0;
+    double d;
+    scanf("%lf",&d);
+    for(int i = 0; i <= d*2;i+=2){
+        x += tan(i);
+    }
+    printf("%f",x);
+    
+}
