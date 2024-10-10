3fc8f6104874e813243d511386f5ea1c31509fb7
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-10-10 15:14:08 +0700

    Update 3.c

--- proga/lab-5.c/3.c
+++ proga/lab-5.c/3.c
@@ -1,28 +1,38 @@
-
 #include <stdio.h>
-#include <math.h>
+#include <stdbool.h>
 
-int main()
-{
-    int a;
-    double i =2.0;
-    int k = 0;
-    int sum = 0;
-    scanf("введите чисдо %d",&a);
-    do{
-
-        double x = fmod(i,2);
-        if((x != 0) || (i == 2)){
-            k += 1;
-        }else k = 0;
-        sum +=k;
-        i++;
-        
-    }while(i <= a);
-    printf("%d", sum);
+int main() {
+    int n;
     
+    printf("Введите число n: ");
+    scanf("%d", &n);
 
+   
+    if (n < 2) {
+        printf("Введенное число должно быть больше или равно 2.\n");
+        return 1;
+    }
+
+    printf("Простые числа в диапазоне от 2 до %d:\n", n);
+
+    for (int i = 2; i <= n; i++) {
+        bool является_простым = true;
+        int j = 2;
+      
+        do {
+            if (i % j == 0 && i != j) {
+                является_простым = false;
+                break; 
+            }
+            j++;
+        } while (j * j <= i);
+
+        if (является_простым) {
+            printf("%d ", i); 
+        }
+    }
+
+    printf("\n");
     
-
+    return 0;
 }
-
