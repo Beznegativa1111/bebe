9d1a44906a083ee45063e8d48586f04bf02444d2
Beznegativa1111 <gigabyte.20251@gmail.com>
2024-11-14 02:03:50 +0700

    Update 1.c

--- proga/lab-10/1.c
+++ proga/lab-10/1.c
@@ -1,29 +1,58 @@
-// Online C compiler to run C program online
+
+
 #include <stdio.h>
 #include <string.h>
-#define MAX_WORDS 100
-#define MAX_LEN 50
-int main() {
-    char *string[MAX_WORDS]={
-    "подоконник",
-    "окно",
-    "холодильник"
-    "подоконник",
-    "пододеяльник"
-    };
-    char mass[MAX_WORDS] = "";
-    char nw[MAX_LEN];
-    printf("введите приставку");
-    scanf("%s",nw);
-    for(int i=0;string[i] != NULL;i++){
-        if(strncmp(string[i],nw,strlen(nw)) == 0){
-            strcat(mass,string[i]);
-            strcat(mass," ");
-        }
+#define LENGTH 500
+#define WORDS 500
+int main()
+{
+    char newmas[WORDS][LENGTH];
+    char string[LENGTH];
+    char slovo[LENGTH];
+    char pristavka[LENGTH];
+    printf("введите строку:  ");
+    fgets(string,sizeof(string),stdin);
+    size_t len = strlen(string);
+      if (len > 0 && string[len - 1] == '\n') {
+        string[len - 1] = '\0';
     }
-    if (strlen(mass) > 0) {
-        printf("Слова, начинающиеся с приставки \"%s\": %s\n", nw, mass);
+    printf("введите приставку:  ");
+    scanf("%s",pristavka);
+    int i =0;
+    int cnt = 0;
+  
+    while(string[i] != '\0'){
+        int j = 0;
+        while(string[i] != '\0' && string[i] != ' '){
+            slovo[j] = string[i];
+            i++;
+            j++;
+        }
+        slovo[j] = '\0';
     
-
+        if(strncmp(slovo,pristavka,strlen(pristavka)) == 0){
+            if(cnt < WORDS){
+                strcpy(newmas[cnt],slovo);
+                cnt++;
+            }else{
+                printf("неа");
+                break;
+            }
+              
+        }
+        while(string[i] == ' '){
+            i++;
+        }
+        
+        
+    }
+    if(cnt == 0){
+        printf("не нашел");
+    }else{
+        for (int k = 0; k < cnt; k++) {
+            printf("%s\n", newmas[k]);
+    }
+    }
     return 0;
+    
 }
